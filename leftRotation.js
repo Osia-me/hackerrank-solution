3e1c4a9e541ae60714b9b13d22c0b9724bfb6c44
Osia-me <olga.melnykovych@gmail.com>
2018-11-20 11:33:40 +0100

    left rotation array challenge included

---
+++ leftRotation.js
@@ -0,0 +1,19 @@
+function rotLeft(a, d) {
+    let i = 0;
+    while (i < d) {
+
+        let el = a[0];
+        a.shift();
+        a.push(el);
+        i++
+    }
+    return a;
+}
+
+///////////  Shorter option
+function rotLeft(a, d) {
+    for (let i = 0; i < d; i++) {
+        a.push(a.shift());
+    }
+    return a;
+}
